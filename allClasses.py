863a82d1d0e3cd5b9f44ccd0cb61878d86660c82
mogulders <mogulders@gmail.com>
2019-09-24 00:48:04 -0500

    Liars Dice

--- allClasses.py
+++ allClasses.py
@@ -18,6 +18,13 @@ class Player:
         self.die_wager = 0
         self.player_hand = []
 
+        # number of die in players hand this is for computer purposes
+        self.number_of_die_inHand = 0
+        # required minimum quantity wager this is for computer purposes
+        self.minimum_quantity_wager = 0
+        # percent of the unsure dice that must be applicable to wager this is for computer purposes
+        self.percent_of_unsure_must_be_applicable = 0.000
+
     def fill_hand(self):
         while len(self.player_hand) < 4:
             dice = Dice()
@@ -37,37 +44,32 @@ class Player:
                 if dice.rolled_number == game.die_wager:
                     game.die_wager_counter += 1
                 dice_on_table_list.append(dice.rolled_number)
-                # if dice.rolled_number == game.die_wager:
-                #     game.die_wager_counter += 1
 
         for player in game.active_players:
             for dice in player.player_hand:
                 if dice.rolled_number == game.die_wager:
                     game.die_wager_counter += 1
                 dice_on_table_list.append(dice.rolled_number)
-                # if dice.rolled_number == game.die_wager:
-                #     game.die_wager_counter += 1
 
         print('The dice on the table are:')
         print(*dice_on_table_list)
 
-        if game.quantity_wager >= game.die_wager_counter:
-            #this one prints twice and removes two dice for some reason
+        if game.quantity_wager > game.die_wager_counter:
             bullshitter = game.active_players[0]
             bullshitter.player_hand.pop(0)
             print(f'{bullshitter.name} loses a dice')
-        elif game.quantity_wager < game.die_wager_counter:
-            #this one works
+        elif game.quantity_wager <= game.die_wager_counter:
             wageree = game.active_players[1]
             wageree.player_hand.pop(0)
             print(f'{wageree.name} loses a dice')
 
         game.reset_players_lists()
         game.clear_wagers()
-        for player in game.players:
-            player.clear_player_wagers()
+        print('')
+        # for player in game.players:
+        #     player.clear_player_wagers()
 
-    #added this in today
+
     def clear_player_wagers(self):
 
         self.die_wager = 0
@@ -95,7 +97,7 @@ class UserPlayer(Player):
 
         while ok_wager == False:
 
-            quantity_wager = int(input("What quantity would you like to wager? You will enter the die next."))
+            quantity_wager = int(input("You will enter the die next. What quantity would you like to wager?"))
             die_wager = int(input("What die would you like to wager?"))
 
             if quantity_wager > game.quantity_wager:
@@ -128,15 +130,18 @@ class UserPlayer(Player):
         redo_answer = True
         while redo_answer:
             response = input("Would you like to Wager or call Bullshit? Enter w for wager Enter b for bullshit")
-            if response == 'b':
+            if response.lower() == 'b':
                 self.call_bullshit(game)
                 redo_answer = False
-            elif response == 'w':
+            elif response.lower() == 'w':
                 self.wager(game)
                 redo_answer = False
             else:
                 print("Invalid answer choose either w for wager or b for bullshit.")
 
+    def clear_die_counter(self):
+        # this is here so it can be called on all instances of player maybe I should put it in Parent class
+        pass
 
 
 class ComputerPlayer(Player):
@@ -154,28 +159,28 @@ class ComputerPlayer(Player):
         self.die5 = [0, 0, 5]
         self.die6 = [0, 0, 6]
 
-        #number of die in players hand
-        self.number_of_die_inHand = 0
 
         # list of die lists
         self.die_list = [self.die1, self.die2, self.die3, self.die4, self.die5, self.die6]
 
-        #required minimum quantity wager
-        self.minimum_quantity_wager = 0
-
         # threshold percentage for wager
         self.threshold_percent = 0.45
-
-        # percent of the unsure dice that must be applicable to wager
-        self.percent_of_unsure_must_be_applicable = 0.000
 
         # initializes everything from the parent class Player
         super().__init__()
 
     def count_dice(self):
+        self.number_of_die_inHand = 0
+        # self.die1 = [0, 0, 1]
+        # self.die2 = [0, 0, 2]
+        # self.die3 = [0, 0, 3]
+        # self.die4 = [0, 0, 4]
+        # self.die5 = [0, 0, 5]
+        # self.die6 = [0, 0, 6]
         # counts the number of each dice in the hand
         # put in decimals to add value to a 6 (ie. wager on a six before a 1)
         for dice in self.player_hand:
+
             self.number_of_die_inHand += 1
             if dice.rolled_number == 1:
                 self.die1[0] += 1
@@ -197,10 +202,11 @@ class ComputerPlayer(Player):
                 self.die6[1] += 1.06
 
     def find_max(self):
-        self.count_dice()
+
         die_counter_list = []
         for die in self.die_list:
             die_counter_list.append(die[1])
+
         max_counter = max(die_counter_list)
         for die in self.die_list:
             if die[1] == max_counter:
@@ -234,6 +240,11 @@ class ComputerPlayer(Player):
         percent_of_unsure_must_be_applicable = cover_amount/unsure_dice
         self.percent_of_unsure_must_be_applicable = percent_of_unsure_must_be_applicable
 
+    def clear_die_counters(self):
+        for die in self.die_list:
+            die[0] = 0
+            die[1] = 0
+
     def wager(self, game):
         self.calculate_the_minimum_wager_quantity(game)
         self.calculate_odds_of_required_wager(game)
@@ -242,24 +253,22 @@ class ComputerPlayer(Player):
         # required quantity wager for wager
         minimum_quantity_wager = self.minimum_quantity_wager
 
-        # I don't think the print statements are working?
         # setting the game quantity wager to the cover amount
         game.quantity_wager = minimum_quantity_wager
         self.quantity_wager = minimum_quantity_wager
         print(f'{self.name} quantity wager: {minimum_quantity_wager}')
-
 
         # setting the game die wager to the number of the dice the comp poses the most of
         game.die_wager = max_die[2]
         self.die_wager = max_die[2]
         print(f'{self.name} die wager: {max_die[2]}')
 
+        self.clear_die_counters()
         game.add_next_active_player()
-
 
     def decide(self, game):
 
-        if self.percent_of_unsure_must_be_applicable > self.threshold_percent:
+        if self.percent_of_unsure_must_be_applicable >= self.threshold_percent:
             self.call_bullshit(game)
         elif self.percent_of_unsure_must_be_applicable < self.threshold_percent:
             self.wager(game)
@@ -272,13 +281,6 @@ class ComputerPlayer(Player):
         self.decide(game)
 
 
-    def clear_die_counter(self):
-        self.number_of_die_inHand = 0
-        for die in self.die_list:
-            die[0] = 0
-            die[1] = 0
-
-
 class Game:
 
     def __init__(self):
@@ -288,7 +290,6 @@ class Game:
         self.die_wager_counter = 0
         self.players = []
         self.active_players = []
-
 
     def fill_players(self):
         player_names_list = ['Bugs Bunny', 'Daffy Duck', 'Marvin The Martian']
@@ -321,12 +322,11 @@ class Game:
                 self.total_dice_on_table += 1
         print(f'{self.total_dice_on_table} dice are on the table')
 
-
     def check_players_elgibility(self):
         for player in self.players:
-            if len(player.player_hand) <= 0:
+            if len(player.player_hand) == 0:
                 print(f'{player.name} is out of dice')
-                self.players.pop(player)
+                self.players.remove(player)
 
     def choose_active_players(self):
 
@@ -335,12 +335,18 @@ class Game:
             self.active_players.append(popped_player)
 
     def add_next_active_player(self):
-        # adds a player to the active players list and then pops the first player that,
-        # should have completed it's turn, little concerned about the timing of it all may need refactor last part
-
-        while len(self.active_players) < 3:
-            popped_player = self.players.pop(0)
+        if len(self.players) > 0:
+            while len(self.active_players) < 3:
+                popped_player = self.players.pop(0)
+                self.active_players.append(popped_player)
+        elif len(self.players) <= 0:
+            popped_player = self.active_players.pop(0)
             self.active_players.append(popped_player)
+
+    def remove_old_active_player(self):
+        while len(self.active_players) > 2:
+            popped_player = self.active_players.pop(0)
+            self.players.append(popped_player)
 
     def reset_players_lists(self):
 
@@ -348,39 +354,25 @@ class Game:
             popped_player = self.active_players.pop(0)
             self.players.append(popped_player)
 
-    def remove_old_active_player(self):
-        while len(self.active_players) > 2:
-            #maybe we make it ad next player and remove previous player seperate methods to give us more flexibility
-            popped_player = self.active_players.pop(0)
-            self.players.append(popped_player)
-
     def set_first_wager(self, game):
         if self.die_wager == 0 and self.quantity_wager == 0:
             player = self.active_players[0]
             player.wager(game)
+            if len(self.players) <= 0:
+                self.add_next_active_player()
 
+    def play_out_round(self, game):
 
-    # def play_round(self, game):
-    #
-    #     # plays a round
-    #     self.all_players_roll()
-    #     self.choose_active_players()
-    #     self.calculate_dice_left()
-    #     self.check_players_elgibility()
-    #
-    #     while self.die_wager == 0 and self.quantity_wager == 0:
-    #         for player in self.active_players:
-    #             player.wager(game)
-    #
-    #     while self.die_wager != 0 or self.quantity_wager != 0:
-    #         for player in self.active_players:
-    #             player.wager_or_bullshit(game)
-    #             """go through and double check the choose active players is working right and then theyre being called in the right spots"""
-    #             self.check_players_elgibility()
-    #             self.add_next_active_player() # added this in today
-    #             self.remove_old_active_player() # added this in today
-    #
-    #     self.reset_players_lists()
+        while self.die_wager != 0 or self.quantity_wager != 0:
+            active_player = self.active_players[1]
+            previous_active_player = self.active_players[0]
+            active_player.wager_or_bullshit(game)
+            if previous_active_player.quantity_wager < active_player.quantity_wager and len(self.players) > 0:
+                self.remove_old_active_player()
+            elif previous_active_player.quantity_wager == active_player.quantity_wager and len(self.players) > 0:
+                if previous_active_player.die_wager < active_player.die_wager:
+                    self.remove_old_active_player()
+
 
     def play_round(self, game):
 
@@ -389,18 +381,10 @@ class Game:
         self.choose_active_players()
         self.calculate_dice_left()
         self.set_first_wager(game)
-
-        while self.die_wager != 0 or self.quantity_wager != 0:
-            active_player = self.active_players[1]
-            previous_active_player = self.active_players[0]
-            active_player.wager_or_bullshit(game)
-            if previous_active_player.quantity_wager < active_player.quantity_wager:
-                self.remove_old_active_player()
-            elif previous_active_player.quantity_wager == active_player.quantity_wager:
-                if previous_active_player.die_wager < active_player.die_wager:
-                    self.remove_old_active_player()
-
+        self.play_out_round(game)
         self.check_players_elgibility()
+        self.clear_variables_in_players()
+
 
     def clear_wagers(self):
         self.quantity_wager = 0
@@ -408,8 +392,19 @@ class Game:
         self.total_dice_on_table = 0
         self.die_wager_counter = 0
 
+    def clear_variables_in_players(self):
+
+        for player in self.players:
+            player.number_of_die_inHand = 0
+            player.minimum_quantity_wager = 0
+            player.percent_of_unsure_must_be_applicable = 0
+
+
     def play_game(self, game):
+
         while len(self.players) > 1:
             self.play_round(game)
+            # for player in self.players:
+            #     player.clear_die_counter()
         for player in self.players:
             print(f'{player.name} won the game!')
